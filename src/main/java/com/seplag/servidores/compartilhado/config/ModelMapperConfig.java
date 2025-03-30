53ad9530b48f2c0aa32c2b0ac956ff27c3c2351c
Rafael Cardoso <rafael.cardoso@pontaagro.com>
2025-03-30 16:43:21 -0300

    chore: adiciona ModelMapper para facilitar mapeamento entre Entidade e DTO

---
+++ src/main/java/com/seplag/servidores/compartilhado/config/ModelMapperConfig.java
@@ -0,0 +1,23 @@
+package com.seplag.servidores.compartilhado.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.config.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+
+        modelMapper
+                .getConfiguration()
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(Configuration.AccessLevel.PRIVATE);
+
+        return modelMapper;
+    }
+
+}
