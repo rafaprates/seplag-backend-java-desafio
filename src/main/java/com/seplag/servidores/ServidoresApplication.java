5fc49e02ebcf0148b44a4365b0977a5a72490131
Rafael Cardoso <rafael.cardoso@pontaagro.com>
2025-03-30 11:08:20 -0300

    chore: emite token JWT ao fazer login por BasicAuthentication

--- src/main/java/com/seplag/servidores/ServidoresApplication.java
+++ src/main/java/com/seplag/servidores/ServidoresApplication.java
@@ -1,13 +1,16 @@
 package com.seplag.servidores;
 
+import com.seplag.servidores.security.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class ServidoresApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServidoresApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServidoresApplication.class, args);
+    }
 
 }
--- src/main/java/com/seplag/servidores/security/config/SecurityConfig.java
+++ src/main/java/com/seplag/servidores/security/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.seplag.servidores.security.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -13,7 +14,9 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
