410c030bd49023385225b4312f4d0e9c3c88accb
Rafael Cardoso <rafael.cardoso@pontaagro.com>
2025-03-31 20:48:11 -0300

    chore: adiciona infraestrutura e código auxiliar para manipular arquivos com MinIO

--- src/main/java/com/seplag/servidores/compartilhado/StartupService.java
+++ src/main/java/com/seplag/servidores/compartilhado/StartupService.java
@@ -4,9 +4,14 @@ import com.seplag.servidores.security.entities.Role;
 import com.seplag.servidores.security.entities.Usuario;
 import com.seplag.servidores.security.services.RoleService;
 import com.seplag.servidores.security.services.UserDetailsService;
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
+import io.minio.MinioClient;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
@@ -16,12 +21,23 @@ import java.util.Set;
 @RequiredArgsConstructor
 public class StartupService {
 
+    @Value("${bucket.buckets.fotos-servidores.nome}")
+    private String fotoBucketNome;
+
     private final UserDetailsService userDetailsService;
     private final RoleService roleService;
+    private final MinioClient minioClient;
 
     @PostConstruct
     public void onStartup() {
         log.info("Rodando rotina de inicialização do sistema");
+
+        criarUsuarioPadrao();
+        criarBucketMinio();
+    }
+
+    private void criarUsuarioPadrao() {
+        log.info("Criando usuário padrão do sistema");
 
         Role rolePadrao = new Role("ROLE_USER");
         roleService.create(rolePadrao);
@@ -33,6 +49,18 @@ public class StartupService {
         );
 
         userDetailsService.create(usuarioPadrao);
+    }
+
+    @SneakyThrows
+    private void criarBucketMinio() {
+        log.info("Criando bucket {} para armazenamento de fotos dos Servidores", fotoBucketNome);
+        boolean exists = minioClient.bucketExists(BucketExistsArgs.builder().bucket(fotoBucketNome).build());
+
+        if (!exists) {
+            minioClient.makeBucket(MakeBucketArgs.builder().bucket(fotoBucketNome).build());
+        } else {
+            log.info("Bucket {} já existe", fotoBucketNome);
+        }
     }
 
 }
---
+++ src/main/java/com/seplag/servidores/compartilhado/config/MinioConfig.java
@@ -0,0 +1,28 @@
+package com.seplag.servidores.compartilhado.config;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinioConfig {
+
+    @Value("${bucket.credentials.username}")
+    private String username;
+
+    @Value("${bucket.credentials.password}")
+    private String password;
+
+    @Value("${bucket.url}")
+    private String bucketUrl;
+
+    @Bean
+    public MinioClient minIO() {
+        return MinioClient.builder()
+                .endpoint(bucketUrl)
+                .credentials(username, password)
+                .build();
+    }
+
+}
